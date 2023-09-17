78455ff4de17f9689f000ae12c35a455ac25b09f
cchen-cc <44825962+cchen-cc@users.noreply.github.com>
2023-09-17 16:10:39 -0400

    Create dataset_split.cmd

---
+++ preprocessing/dataset_split.cmd
@@ -0,0 +1,17 @@
+## BTCV dataset
+* Data split: test_fd_list = ['0035', '0036', '0037', '0038', '0039', '0040']
+
+## Prostates datasets
+* Data split
+  * SiteA: test_fd_list = ['0003', '0008', '0012', '0015', '0018', '0026']
+  * SiteB: test_fd_list = ['0003', '0008', '0012', '0015', '0018', '0026']
+  * SiteC: test_fd_list = ['0003', '0008', '0013', '0015']
+  * SiteD: test_fd_list = ['0001', '0032', '0034']
+  * SiteE: test_fd_list = ['0000', '0004', '0009']
+  * SiteF: test_fd_list = ['0038', '0041', '0046']
+
+## EndoVis18
+* Data split: test_fd_list = released test set ['seq_1', 'seq_2', 'seq_3', 'seq_4']
+
+## MSD-Pancreas
+* Data split: data is splitted according to [this file](). 
